6f998c1a54c06c111828fb2a7f192202f61dba0f
Raj Hamal <raajhamal7@gmail.com>
2025-08-11 14:04:04 +0545

    Update day1_queries.sql

--- notebooks/day1_queries.sql
+++ notebooks/day1_queries.sql
@@ -1,1 +1,12 @@
+SELECT  
+FROM `bellabeat-capstone-468707.bellabeat_data.daily_acitivity` LIMIT 10
 
+SELECT 
+ COUNT(DISTINCT ID) AS total_users
+ FROM `bellabeat-capstone-468707.bellabeat_data.daily_acitivity`;
+
+SELECT Id,
+       ROUND(AVG(TotalSteps), 0) AS avg_daily_steps
+FROM `bellabeat-capstone-468707.bellabeat_data.daily_acitivity`
+GROUP BY Id
+ORDER BY avg_daily_steps DESC;
